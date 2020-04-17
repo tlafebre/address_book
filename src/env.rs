0857c845e26bceed3fa1383513c4ddccc7c499a9
Lafebre <Tjeerd.Lafebre@ing.com>
2020-04-17 12:43:51 +0200

    fixed home_dir; .db file is now created in correct location

--- src/env.rs
+++ src/env.rs
@@ -1,15 +1,11 @@
 use std::path::PathBuf;
 
-pub fn current_dir() -> PathBuf {
-    std::env::current_dir()
-        .unwrap()
-        .parent()
-        .unwrap()
-        .to_path_buf()
+pub fn home_dir() -> PathBuf {
+    std::env::current_dir().unwrap().to_path_buf()
 }
 
 pub fn data_dir() -> PathBuf {
-    let mut data_dir = current_dir();
+    let mut data_dir = home_dir();
     data_dir.push("data");
 
     data_dir
--- todo.md
+++ todo.md
@@ -1,5 +1,8 @@
 [x] create db when none is present
-[ ] list should present results alphabetically sorted
+[ ] db should be created in data dir, which is in the home_dir
+[ ] list should present results alphabetically sorted by last name, then first name
+[ ] names should be inserted capitalized
+[ ] non ascii
 [ ] duplicate entries by hashing all columns?
 [ ] empty fields are not allowed
 [ ] date of birth -> age
